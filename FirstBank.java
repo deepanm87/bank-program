67aa0baf1364e7c0650037741cba7be81dfa26a9
deepanm87 <deepan.murugan29@gmail.com>
2022-08-24 15:03:09 -0400

    added account.java and firstbank.java

---
+++ FirstBank.java
@@ -0,0 +1,12 @@
+public class FirstBank {
+  public static void main(String[] args) {
+
+    Account deep = new Account("Deep Morgan", "0951717");
+    deep.showMenu();
+
+
+    Account ram = new Account("Ram Stevens", "0158986");
+    //ram.showMenu();
+
+  }
+}
